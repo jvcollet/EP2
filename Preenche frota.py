3a981e380eaceadd273fb7f6e283fd67810ef1c0
João Vitor Collet <jvcollet@gmail.com>
2023-04-27 15:00:48 -0300

    Preenche frota

---
+++ Preenche frota.py
@@ -0,0 +1,19 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    lista_posicoes= []
+    if orientacao=="vertical":
+        for posicao in range(tamanho):
+            lista_posicoes.append([linha+posicao, coluna])
+    elif orientacao == 'horizontal':
+        for posicao in range(tamanho):
+            lista_posicoes.append([linha, coluna+posicao])
+    return lista_posicoes
+   
+
+def preenche_frota(frota, nome, linha, coluna, orientacao, tamanho):
+    cordenadas= define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome not in frota:
+        frota[nome]= [cordenadas]
+    else:
+        frota[nome].append(cordenadas)
+    return frota
+
