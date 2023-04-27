b546c06f61d33b92045618f2ef2fa7aaec4836a8
henriquetiberio <henriquect@al.insper.edu.br>
2023-04-27 14:40:17 -0300

    Primeiro código

---
+++ 1.py
@@ -0,0 +1,10 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    lista_posicoes= []
+    if orientacao=="vertical":
+        for posicao in range(tamanho):
+            lista_posicoes.append([linha+posicao, coluna])
+    elif orientacao == 'horizontal':
+        for posicao in range(tamanho):
+            lista_posicoes.append([linha, coluna+posicao])
+    return lista_posicoes
+   
